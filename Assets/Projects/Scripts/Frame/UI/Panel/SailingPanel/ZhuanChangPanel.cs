445748748dcd6be70a187393a45aa0ce21ba429d
oubaoyao <673411498@qq.com>
2020-06-23 17:30:41 +0800

    1、铺管按钮随着重置按钮复原

--- Assets/Projects/Scripts/Frame/UI/Panel/SailingPanel/ZhuanChangPanel.cs
+++ Assets/Projects/Scripts/Frame/UI/Panel/SailingPanel/ZhuanChangPanel.cs
@@ -182,7 +182,7 @@ public class ZhuanChangPanel : BasePanel
 
             AutoDrive.Reset();
             ManualDrive.Reset();
-
+            sailingPanel.PuguanPanel.Reset();
         });
     }
 
